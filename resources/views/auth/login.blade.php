c01a1c83371770c28dd3f95b96b61509c0e448e1
Verónica Flores Gutiérrez <veflo13@gmail.com>
2023-01-27 14:34:24 +0100

    background login in progress

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,8 +4,9 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+        <div class="text-center">{{ __('Hola!') }}</div>
+            <div class="card bg-success p-2 text-dark bg-opacity-25">
+                
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
@@ -39,29 +40,19 @@
                             </div>
                         </div>
 
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
+                        <div class="row  mb-0">
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
+                                    {{ __('Ingresar') }}
                                 </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
+                                <br>
+                                    @if (Route::has('password.request'))
+                                        <a class="btn btn-link" href="{{ route('password.request') }}">
+                                            {{ __('olvidaste tu contraseña?') }}
+                                        </a>
+                                    @endif
+                                </br>
                             </div>
                         </div>
                     </form>
