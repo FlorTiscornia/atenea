1677d4c29b18c2216940f87e191ca87667fca5aa
FlorTiscornia <flora.tiscornia@gmail.com>
2023-02-01 13:06:41 +0100

    css footer

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -3,7 +3,7 @@
         <div class="copyRight">&copy;Copyright 2023</div>
     </div>
     
-        <div class="smTags">    
+    <div class="smTags">    
         <img src="https://res.cloudinary.com/de1i08drf/v1675247890/Atenea/logoMinervaBlancoSmall_tjplma.png">
-        </div>
+    </div>
 </footer>
