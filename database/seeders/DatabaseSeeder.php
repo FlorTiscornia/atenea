faef64365c55ff8be09160df3c670fd91c9065b7
LolaGM <canal030w@gmail.com>
2023-02-02 15:35:23 +0100

    changed little details about students

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,9 +24,9 @@ class DatabaseSeeder extends Seeder
         // ]);
 
         User::factory()->create([
-            'name' => 'Elena',
+            'name' => 'Elena Mª',
             'email' => 'elenarjonap@gmail.com',
-            'lastname' => 'Perez',
+            'lastname' => 'Pérez Arjona',
             'img' =>'https://st2.depositphotos.com/2703645/6084/v/950/depositphotos_60844877-stock-illustration-woman-character-avatar.jpg',
             'group' => 1,
             'isTeacher' => false
@@ -35,7 +35,7 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Lola G',
             'email' => 'canal030w@gmail.com',
-            'lastname' => 'Garcia',
+            'lastname' => 'García Morcillo',
             'img' =>'https://st2.depositphotos.com/2703645/12072/v/950/depositphotos_120725498-stock-illustration-woman-avatar-character.jpg',
             'group' => 1,
             'isTeacher' => false
@@ -44,7 +44,7 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Carmen G',
             'email' => 'carmengallardopozo@gmail.com',
-            'lastname' => 'Gallardo',
+            'lastname' => 'Gallardo Pozo',
             'img' =>'https://st2.depositphotos.com/2703645/11507/v/950/depositphotos_115077884-stock-illustration-woman-avatar-character.jpg',
             'group' => 1,
             'isTeacher' => false
@@ -61,7 +61,7 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Lola N',
             'email' => 'lolanavalop@gmail.com',
-            'lastname' => 'Navarro',
+            'lastname' => 'Navarro López',
             'img' =>'https://st2.depositphotos.com/2703645/6084/v/950/depositphotos_60844045-stock-illustration-woman-character-avatar.jpg',
             'group' => 1,
             'isTeacher' => false
@@ -69,7 +69,7 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Alesia',
             'email' => 'alesia.nekolethal@gmail.com',
-            'lastname' => 'Nekolethal',
+            'lastname' => 'Baldeón Machuca',
             'img' =>'https://st2.depositphotos.com/1006318/5909/v/600/depositphotos_59094217-stock-illustration-businesswoman-profile-icon.jpg',
             'group' => 1,
             'isTeacher' => false
@@ -77,7 +77,7 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Carmen C',
             'email' => 'karmelacfer@gmail.com',
-            'lastname' => 'Cruces',
+            'lastname' => 'Cruces Fernández',
             'img' =>'https://st2.depositphotos.com/2703645/6084/v/950/depositphotos_60848463-stock-illustration-woman-avatar.jpg',
             'group' => 1,
             'isTeacher' => false
@@ -85,7 +85,7 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Esther',
             'email' => 'esrherdlz1215@gmail.com',
-            'lastname' => 'xxx',
+            'lastname' => 'De Luque Zurita ',
             'img' =>'https://st2.depositphotos.com/2703645/11476/v/950/depositphotos_114764976-stock-illustration-woman-avatar-character.jpg',
             'group' => 1,
             'isTeacher' => false
@@ -93,7 +93,7 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Paloma R',
             'email' => 'ruizpaloma9@gmail.com',
-            'lastname' => 'Ruiz',
+            'lastname' => 'Ruiz Ramírez',
             'img' =>'https://st2.depositphotos.com/2703645/12072/v/950/depositphotos_120726078-stock-illustration-woman-avatar-character.jpg',
             'group' => 1,
             'isTeacher' => false
@@ -102,14 +102,14 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Paloma B',
             'email' => 'palomita@gmail.com',
-            'lastname' => 'Babot',
+            'lastname' => 'Babot León',
             'img' =>'https://st2.depositphotos.com/2703645/6084/v/950/depositphotos_60845379-stock-illustration-woman-character-avatar.jpg',
             'group' => 1,
             'isTeacher' => false
         ]);
 
         User::factory()->create([
-            'name' => 'Veronica',
+            'name' => 'VeroniKa',
             'email' => 'komar2002@gmail.com',
             'lastname' => 'Komarova',
             'img' =>'https://st2.depositphotos.com/19450650/42951/v/1600/depositphotos_429519474-stock-illustration-woman-silhouette-portrait-vector-illustration.jpg',
@@ -120,7 +120,7 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Sandra',
             'email' => 'sandralilder@gmail.com',
-            'lastname' => 'Leon',
+            'lastname' => 'Leon del Risco',
             'img' =>'https://st2.depositphotos.com/2703645/6084/v/950/depositphotos_60848263-stock-illustration-woman-avatar.jpg',
             'group' => 1,
             'isTeacher' => false
@@ -128,7 +128,7 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Raquel',
             'email' => 'palomofuentesraquel2002@gmail.com',
-            'lastname' => 'Palomo',
+            'lastname' => 'Palomo Fuentes',
             'img' =>'https://st2.depositphotos.com/2703645/11476/v/950/depositphotos_114763844-stock-illustration-woman-avatar-character.jpg',
             'group' => 1,
             'isTeacher' => false
@@ -136,15 +136,15 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Sierri',
             'email' => 'sierri.peri@gmail.com',
-            'lastname' => 'Perez',
+            'lastname' => 'Pérez Sánchez',
             'img' =>'https://st2.depositphotos.com/2703645/12072/v/950/depositphotos_120728726-stock-illustration-woman-avatar-character.jpg',
             'group' => 1,
             'isTeacher' => false
         ]);
         User::factory()->create([
-            'name' => 'Himorell',
+            'name' => 'Himo',
             'email' => 'himorellj@gmail.com',
-            'lastname' => 'Jaramillo',
+            'lastname' => 'Jaramillo González ',
             'img' =>'https://st2.depositphotos.com/2703645/11476/v/950/depositphotos_114764242-stock-illustration-woman-avatar-character.jpg',
             'group' => 1,
             'isTeacher' => false
@@ -158,9 +158,9 @@ class DatabaseSeeder extends Seeder
             'isTeacher' => false
         ]);
         User::factory()->create([
-            'name' => 'Veronica F',
+            'name' => 'Verónica F',
             'email' => 'veflo13@gmail.com',
-            'lastname' => 'Flores',
+            'lastname' => 'Flores Gutiérrez',
             'img' =>'https://st2.depositphotos.com/2703645/11438/v/950/depositphotos_114387610-stock-illustration-woman-avatar-character.jpg',
             'group' => 1,
             'isTeacher' => false
@@ -168,7 +168,7 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Adriana',
             'email' => 'agruad@gmail.com',
-            'lastname' => 'Aguilar',
+            'lastname' => 'Aguilar Ruiz',
             'img' =>'https://st2.depositphotos.com/1006318/5909/v/950/depositphotos_59094623-stock-illustration-female-avatar-woman.jpg',
             'group' => 1,
             'isTeacher' => false
@@ -176,7 +176,7 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Beatriz',
             'email' => 'beadeavila10@gmail.com',
-            'lastname' => 'De Avila',
+            'lastname' => 'De Ávila Jiménez',
             'img' =>'https://st4.depositphotos.com/2703645/24892/v/1600/depositphotos_248920644-stock-illustration-colorful-vector-illustration-woman-avatar.jpg',
             'group' => 1,
             'isTeacher' => false
@@ -184,7 +184,7 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Noa',
             'email' => 'anamaria.tz@hotmail.com',
-            'lastname' => 'Trujillo',
+            'lastname' => 'Trujillo Zamora',
             'img' =>'https://st2.depositphotos.com/2703645/11438/v/950/depositphotos_114389662-stock-illustration-woman-avatar-character.jpg',
             'group' => 1,
             'isTeacher' => false
@@ -192,7 +192,7 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Ana',
             'email' => 'ana_rg95@hotmail.com',
-            'lastname' => 'Rueda',
+            'lastname' => 'Rueda Guiu',
             'img' =>'https://st2.depositphotos.com/2703645/11326/v/950/depositphotos_113264588-stock-illustration-womaan-avatar-character.jpg',
             'group' => 1,
             'isTeacher' => false
@@ -200,7 +200,7 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Alba',
             'email' => 'albamrus@gmail.com',
-            'lastname' => 'Rus',
+            'lastname' => 'Rus Martínez',
             'img' =>'https://st2.depositphotos.com/2703645/6084/v/950/depositphotos_60848009-stock-illustration-woman-character-avatar.jpg',
             'group' => 1,
             'isTeacher' => false
@@ -208,7 +208,7 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Camila',
             'email' => 'camilaruiz17@gmail.com',
-            'lastname' => 'Ruiz',
+            'lastname' => 'Ruiz Gutiérrez',
             'img' =>'https://st2.depositphotos.com/2703645/6084/v/950/depositphotos_60847945-stock-illustration-woman-character-avatar.jpg',
             'group' => 1,
             'isTeacher' => false
@@ -216,7 +216,7 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Natalia',
             'email' => 'nataliapalomof@gmail.com',
-            'lastname' => 'Palomo',
+            'lastname' => 'Palomo Fuentes',
             'img' =>'https://st2.depositphotos.com/1006318/5909/v/950/depositphotos_59095633-stock-illustration-businesswoman-profile-icon.jpg',
             'group' => 1,
             'isTeacher' => false
@@ -224,7 +224,7 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Paula',
             'email' => 'npagudelo884@gmail.com',
-            'lastname' => 'Agudelo',
+            'lastname' => 'Ramírez Agudelo',
             'img' =>'https://st2.depositphotos.com/2703645/12072/v/950/depositphotos_120724058-stock-illustration-woman-avatar-character.jpg',
             'group' => 1,
             'isTeacher' => false
@@ -233,7 +233,7 @@ class DatabaseSeeder extends Seeder
             'name' => 'JM/Celia',
             'email' => 'celieme@factoriaf5.org',
             'lastname' => 'Factorizados',
-            'img' =>'https://img.freepik.com/foto-gratis/anade-real-rodeado-flores-vegetacion-campo-luz-sol_181624-15686.jpg?w=996&t=st=1674',
+            'img' =>'https://img.freepik.com/vector-premium/pato-goma-amarillo-juguete-bano-bebes_501173-75.jpg?w=826',
             'group' => 1,
             'isTeacher' => true
         ]);
