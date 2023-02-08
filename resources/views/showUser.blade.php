0ace09fe69d689917b3de5b12fc80bb3b51fec47
ZLoln <lolanavalo@gmail.com>
2023-02-08 21:56:13 +0100

    working on User profile style

--- resources/views/showUser.blade.php
+++ resources/views/showUser.blade.php
@@ -29,17 +29,17 @@
             </div>
         </div>
         <div class="accordion" id="accordionPanelsStayOpenExample">
-            <div class="accordion-item">
+            <div  class="accordion-item">
                 <h2 class="accordion-header" id="panelsStayOpen-headingOne">
-                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
+                <button id="trimesterHeader" class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
                     Primer trimestre
                 </button>
                 </h2>
                 <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
-                <div class="accordion-body">
+                <div id="trimesterBody" class="accordion-body">
                 <table class="table">
-                    <thead>
-                        <tr>
+                    <thead id="tableBody">
+                        <tr id="spaceRows">
                         <th></th>
                         <th>I</th>
                         <th>II</th>
@@ -47,36 +47,37 @@
                         <th>Promedio</th>
                         </tr>
                     </thead>
-                    <tbody>
-                        <tr>
-                        <th scope="row">Lengua</th>
+                    <tbody id="tableBody">
+                        <tr id="spaceRows">
+                        <th id="gradesTitle" scope="row">Lengua</th>
                         <td>5</td>
                         <td>5</td>
                         <td>10</td>
                         <td>6.6</td>
                         </tr>
-                        <tr>
-                        <th scope="row">Matemáticas</th>
+                        <tr id="spaceRows">
+                        <th id="gradesTitle" scope="row">Matemáticas</th>
                         <td>8</td>
                         <td>8</td>
                         <td>8</td>
                         <td>8</td>
                         </tr>
-                        <tr>
-                        <th scope="row">Inglés</th>
+                        <tr id="spaceRows">
+                        <th id="gradesTitle" scope="row">Inglés</th>
                         <td>5</td>
                         <td>5</td>
                         <td>10</td>
                         <td>6.6</td>
                         </tr>
-                        <th scope="row">Historia</th>
+                        <tr id="spaceRows">
+                        <th id="gradesTitle" scope="row">Historia</th>
                         <td>10</td>
                         <td>8</td>
                         <td>10</td>
                         <td>9.3</td>
                         </tr>
-                        <tr>
-                        <th scope="row">Geografía</th>
+                        <tr id="spaceRows">
+                        <th id="gradesTitle" scope="row">Geografía</th>
                         <td>4</td>
                         <td>4</td>
                         <td>4</td>
@@ -87,9 +88,9 @@
                 </div>
             </div>
         </div>
-            <div class="accordion-item">
+            <div id="trimesterBody" class="accordion-item">
                 <h2 class="accordion-header" id="panelsStayOpen-headingTwo">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
+                <button id="trimesterHeader" class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
                     Segundo trimestre
                 </button>
                 </h2>
@@ -107,34 +108,34 @@
                     </thead>
                     <tbody>
                         <tr>
-                        <th scope="row">Lengua</th>
+                        <th id="gradesTitle" scope="row">Lengua</th>
                         <td>5</td>
                         <td>5</td>
                         <td>10</td>
                         <td>6.6</td>
                         </tr>
                         <tr>
-                        <th scope="row">Matemáticas</th>
+                        <th id="gradesTitle" scope="row">Matemáticas</th>
                         <td>8</td>
                         <td>8</td>
                         <td>8</td>
                         <td>8</td>
                         </tr>
                         <tr>
-                        <th scope="row">Lengua</th>
+                        <th id="gradesTitle" scope="row">Lengua</th>
                         <td>5</td>
                         <td>5</td>
                         <td>10</td>
                         <td>6.6</td>
                         </tr>
-                        <th scope="row">Historia</th>
+                        <th id="gradesTitle" scope="row">Historia</th>
                         <td>10</td>
                         <td>8</td>
                         <td>10</td>
                         <td>9.3</td>
                         </tr>
                         <tr>
-                        <th scope="row">Geografía</th>
+                        <th id="gradesTitle" scope="row">Geografía</th>
                         <td>4</td>
                         <td>4</td>
                         <td>4</td>
@@ -145,9 +146,9 @@
                 </div>
             </div>
         </div>
-            <div class="accordion-item">
+            <div id="trimesterBody" class="accordion-item">
                 <h2 class="accordion-header" id="panelsStayOpen-headingThree">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
+                <button id="trimesterHeader" class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
                     Tercer trimestre
                 </button>
                 </h2>
@@ -165,34 +166,34 @@
                     </thead>
                     <tbody>
                         <tr>
-                        <th scope="row">Lengua</th>
+                        <th id="gradesTitle" scope="row">Lengua</th>
                         <td>5</td>
                         <td>5</td>
                         <td>10</td>
                         <td>6.6</td>
                         </tr>
                         <tr>
-                        <th scope="row">Matemáticas</th>
+                        <th id="gradesTitle" scope="row">Matemáticas</th>
                         <td>8</td>
                         <td>8</td>
                         <td>8</td>
                         <td>8</td>
                         </tr>
                         <tr>
-                        <th scope="row">Lengua</th>
+                        <th id="gradesTitle" scope="row">Lengua</th>
                         <td>5</td>
                         <td>5</td>
                         <td>10</td>
                         <td>6.6</td>
                         </tr>
-                        <th scope="row">Historia</th>
+                        <th id="gradesTitle" scope="row">Historia</th>
                         <td>10</td>
                         <td>8</td>
                         <td>10</td>
                         <td>9.3</td>
                         </tr>
                         <tr>
-                        <th scope="row">Geografía</th>
+                        <th id="gradesTitle" scope="row">Geografía</th>
                         <td>4</td>
                         <td>4</td>
                         <td>4</td>
@@ -204,7 +205,13 @@
             </div>
         </div>
         <br>
-        Promedio General:<input> 
-        
+        <div id="averageContainer">
+            <div id="averageCard">
+                Promedio General:
+                    <div>
+                        <input id="averageGrade"> 
+                    </div>
+            </div>
+        </div>
 
 @endsection
