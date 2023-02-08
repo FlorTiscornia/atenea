18ff5da762f6cb7afaa787e75dcc00024d82b23d
ZLoln <lolanavalo@gmail.com>
2023-02-08 08:54:11 +0100

    login style restored

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,15 +4,15 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="text-center salute">{{ __('¡Hola!') }}<img src="https://res.cloudinary.com/de1i08drf/
+            <div id="salute" class="text-center">{{ __('¡Hola!') }}<img src="https://res.cloudinary.com/de1i08drf/
 v1675077884/Atenea/cabezaBuhoAzulSmall_piqqgc.png" style="margin-left: 30px">
             </div>
             
             <div id="loginCard" >
                 <div id="cardContainer">
-                    <div class="card bg-success p-2 text-dark bg-opacity-25 ">
+                    <div id="cardBackground" class="card bg-success p-2 text-dark bg-opacity-25 ">
                         
-                        <div class="card-body ">
+                        <div id="studentLog" class="card-body ">
                             <form method="POST" action="{{ route('login') }}">
                                 @csrf
 
@@ -37,14 +37,14 @@ v1675077884/Atenea/cabezaBuhoAzulSmall_piqqgc.png" style="margin-left: 30px">
                                 </span>
                                 @enderror
                                 
-                                <div class="d-grid gap-2 d-md-flex justify-content-md-center">
-                                    <button type="submit" class="btn btn-primary text">
+                                <div id="logForgotContainer" class="d-grid gap-2 d-md-flex justify-content-md-center">
+                                    <button id="loginBttn" type="submit" class="btn btn-primary text">
                                         {{ __('Ingresar') }}
                                     </button>
                                     <br>
                                         @if (Route::has('password.request'))
-                                        <a class="btn btn-link" href="{{ route('password.request') }}">
-                                            {{ __('¿olvidaste la contraseña?') }}
+                                        <a id="forgottenP" class="btn btn-link" href="{{ route('password.request') }}">
+                                            {{ __('¿Olvidaste la contraseña?') }}
                                         </a>
                                         @endif
                                     </br>
